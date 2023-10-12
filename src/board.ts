fe5048ed37b5f3770f4f487257085694ea34f011
bi3mer <colan@comcast.net>
2023-10-12 10:31:14 -0600

    I'm going to do a bit board

--- src/board.ts
+++ src/board.ts
@@ -6,12 +6,35 @@ export class Board {
   // TODO: have connect 3 remove all instances
   // TODO: update animation so it goes top down
   // TODO: add some kind of support for a proper lerp
+  //
+  //
+  //
+  // For each type there is a bitboard
+  //
+  // |-----------------------|
+  // | 6  13  20  27  34  41 | top row
+  // | 5  12  19  26  33  40 |
+  // | 4  11  18  25  32  39 |
+  // | 3  10  17  24  31  38 |
+  // | 2   9  16  23  30  37 |
+  // | 1   8  15  22  29  36 |
+  // | 0   7  14  21  28  35 | bottom row
+  // |-----------------------|
+  //
+  // Meaning, we are not using 42-63 of a 64 bit number in JavaScript.
+  //
   b: number[][]
+  boards: number[]
   isDirty: boolean
   
   constructor() {
     this.isDirty = true;
     this.b = [];
+    for(let i = 0; i < Assets.size; ++i) {
+      this.b.push(0);
+    }
+
+    /*
     for(let y = 0; y < BOARD_HEIGHT; ++y) {
       let temp = [];
       for(let x = 0; x < BOARD_WIDTH; ++x) {
@@ -19,7 +42,7 @@ export class Board {
       }
 
       this.b.push(temp);
-    }
+    }*/
 
     // @debugging
     this.b[BOARD_HEIGHT-1][0] = 1;
@@ -96,7 +119,7 @@ export class Board {
    * @returns number - score
    */
   private findConnect3(): number {
-    let x: number, val: number;
+    let x: number, val: number, i: number;
     let score = 0;
 
     for(let y = 0; y < BOARD_HEIGHT; ++y) {
@@ -104,17 +127,27 @@ export class Board {
         val = this.b[y][x];
         if (val == -1) continue;
         
-        let modX: number, modY: number;
-        for (let modY = 0; y+modY < BOARD_HEIGHT; ++modY) {
-          for(let modX = 0; x+modX < BOARD_WIDTH; ++modX) {
-          }
-        } 
-
+        let neighbors = [];
+        this.floodFill(x, y, neighbors);
+        
+        const size = neighbors.length;
+        for(let i = 0; i < size; ++i) {
+          const [x, y] = neighbors[i];
+          this.b[y][x] = -1;
+        }
       }
     }
 
     return 0;
   }
+  
+  private floodFill(x: number, y: number, neighbors: [number, number][]): void {
+    return; // DOn't want an infinite loop
+    this.floodFill(x+1, y, neighbors);
+    this.floodFill(x-1, y, neighbors);
+    this.floodFill(x, y+1, neighbors);
+    this.floodFill(x, y-1, neighbors);
+  } 
 
   private connect3Exists(): boolean {
     let x: number, mod: number, cur: number;
